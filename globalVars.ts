3eeed65d79b9dd5e9c84ffeb966122cba7917ca5
Toriong <gtorion97@gmail.com>
2023-12-06 12:46:31 -0600

    creating selection View for the GameScrn

--- globalVars.ts
+++ globalVars.ts
@@ -1,1 +1,10 @@
 export const IS_TESTING = true;
+export const ENGLISH_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
+export const SYMBOLS = [
+    "~",
+    "->",
+    "(",
+    ")",
+    "*",
+    "v"
+  ]
